fb66679aec23b20774a70e74fd97aa26df145ee3
atiyeashari <atiyeashari@gmail.com>
2017-11-04 18:11:09 -0400

    Added authentication and database initialization

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from booking.models import *
 
 # Register your models here.
-admin.site.register(Person)
 admin.site.register(BabySitter)
 admin.site.register(Family)
+admin.site.register(BookedTime)
+admin.site.register(Availability)
--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.db import models
 
 # Create your models here.
@@ -6,12 +6,34 @@ from django.db import models
 
 class BabySitter(models.Model):
     user = models.OneToOneField(User, primary_key=True)
-    mobile_phone = models.CharField(null=True, max_length=10, blank=True, help_text='4168804341')
+    phone_number = models.CharField(null=True, max_length=10, blank=True, help_text='4168804341')
+
+    def save(self, *args, **kwargs):
+        user = self.user
+        p = Permission.objects.get(codename='baby_sitter')
+        user.user_permissions.add(p)
+        super(BabySitter, self).save(*args, **kwargs)
+
+    class Meta:
+        permissions = (
+            ("baby_sitter", "Can add availabilities"),
+        )
 
 
 class Family(models.Model):
     user = models.OneToOneField(User, primary_key=True)
-    mobile_phone = models.CharField(null=True, max_length=10, blank=True, help_text='4168804341')
+    phone_number = models.CharField(null=True, max_length=10, blank=True, help_text='4168804341')
+
+    def save(self, *args, **kwargs):
+        user = self.user
+        p = Permission.objects.get(codename='family')
+        user.user_permissions.add(p)
+        super(Family, self).save(*args, **kwargs)
+
+    class Meta:
+        permissions = (
+            ("family", "Can book time spans"),
+        )
 
 
 class TimeSpan(models.Model):
