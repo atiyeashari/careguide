6df678fed16b33aed2e485414f039df70e7875d2
atiyeashari <atiyeashari@gmail.com>
2017-11-06 09:54:41 -0500

    Basic account pages added.

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 import django.contrib.auth.urls
-# from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
-    url(r'^$', views.index, name='index'),
-    # url(r'^login/$', auth_views.login, name='login'),
-    # url(r'^logout/$', auth_views.logout, name='logout'),
+    # url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html', redirect_field_name='index'), name='login'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^accounts/profile/$', views.index, name='index')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url='/login/')
 def index(request):
+    if request and request.user:
+        if request.user.has_perm('booking.baby_sitter'):
+            context = {}
+            return render(request, 'babySitterAccount.html', context)
+        elif request.user.has_perm('booking.family'):
+            context = {}
+            return render(request, 'familyAccount.html', context)
+
     return HttpResponse("Hello, world. You're at the booking index.")
